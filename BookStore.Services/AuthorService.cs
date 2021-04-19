9f0e45d4f13d183a85e54bb2ebc758eb19fa937f
ktugrul <47542064+ktugrul@users.noreply.github.com>
2021-04-19 14:25:15 +0300

    Add files via upload

---
+++ BookStore.Services/AuthorService.cs
@@ -0,0 +1,48 @@
+﻿using BookStore.Core;
+using BookStore.Core.Models;
+using BookStore.Core.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore.Services
+{
+    public class AuthorService : IAuthorService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public AuthorService(IUnitOfWork unitOfWork)
+        {
+            this._unitOfWork = unitOfWork;
+        }
+
+        public async Task<Author> Create(Author author)
+        {
+            await _unitOfWork.Authors.AddAsync(author);
+            return author;
+        }
+
+        public async Task Delete(Author author)
+        {
+            _unitOfWork.Authors.Remove(author);
+            await _unitOfWork.CommitAsync();
+        }
+
+        public async Task<IEnumerable<Author>> GetAllAuthors()
+        {
+            return await _unitOfWork.Authors.GetAllAsync();
+        }
+
+        public async Task<Author> GetAuthorById(int id)
+        {
+            return await _unitOfWork.Authors.GetByIdAsync(id);
+        }
+
+        public async Task Update(Author author, Author updatedAuthor)
+        {
+            updatedAuthor.Name = author.Name;
+            await _unitOfWork.CommitAsync();
+        }
+    }
+}
---
+++ BookStore.Services/BookService.cs
@@ -0,0 +1,56 @@
+﻿using BookStore.Core;
+using BookStore.Core.Models;
+using BookStore.Core.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore.Services
+{
+    public class BookService : IBookService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public BookService(IUnitOfWork unitOfWork)
+        {
+            this._unitOfWork = unitOfWork;
+        }
+
+
+        public async Task<Book> Create(Book book)
+        {
+            await _unitOfWork.Books.AddAsync(book);
+            await _unitOfWork.CommitAsync();
+            return book;
+        }
+
+        public async Task Delete(Book book)
+        {
+            _unitOfWork.Books.Remove(book);
+            await _unitOfWork.CommitAsync();
+        }
+
+        public async Task<IEnumerable<Book>> GetAllWithAuthors()
+        {
+            return await _unitOfWork.Books.GetAllWithAuthorAsync();
+        }
+
+        public async Task<Book> GetBookById(int id)
+        {
+            return await _unitOfWork.Books.GetWithAuthorByIdAsync(id);
+        }
+
+        public async Task<IEnumerable<Book>> GetBooksByArtistId(int authorId)
+        {
+            return await _unitOfWork.Books.GetAllWithAuthorByAuthorIdAsync(authorId);
+        }
+
+        public async Task Update(Book book, Book updatedBook)
+        {
+            updatedBook.Name = book.Name;
+            updatedBook.AuthorId = book.AuthorId;
+
+            await _unitOfWork.CommitAsync();
+        }
+    }
+}
